a6c2e0206c57bc66da1f3fe76f00da5eacd0510d
romen95 <romanretinskiy@yandex.ru>
2023-03-13 02:28:00 +0300

    fix feedback

--- src/main/resources/static/js/formMail.js
+++ src/main/resources/static/js/formMail.js
@@ -1,6 +1,35 @@
-$("#sendMail").on("click", function () {
-    var name = $("#name").val();
-    var phone = $("#phone").val();
-    var message = $("#message").val();
-    var checkbox = $("#checkbox").val();
+$("#sendMail").on("click", function() {
+    var name = $("#name").val().trim();
+    var phone = $("#phone").val().trim();
+    var message = $("#message").val().trim();
+    var checkbox = $("#checkbox");
+
+    if (name == "") {
+        $("#errorMess").text("Введите имя");
+        return false;
+    } else if (phone == "") {
+        $("#errorMess").text("Введите номер телефона");
+        return false;
+    } else if (!(checkbox.is(':checked'))) {
+        $("#errorMess").text("Поставьте галочку");
+        return false;
+    }
+
+    $("#errorMess").text("");
+
+    $.ajax({
+        url: 'mail.php',
+        type: 'POST',
+        cache: false,
+        data: { 'name': name, 'phone': phone },
+        dataType: 'html',
+        beforeSend: function() {
+          $("#sendMail").prop("disabled", true);
+        },
+        success: function(data) {
+            alert(data);
+            $("#sendMail").prop("disabled", false);
+        }
+    });
+
 });
