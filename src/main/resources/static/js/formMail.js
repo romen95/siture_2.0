dd5a35ce9dc9a7b23b88762136d34d2c2cec2fc0
romen95 <romanretinskiy@yandex.ru>
2023-03-13 00:15:52 +0300

    add popup feedback

---
+++ src/main/resources/static/js/formMail.js
@@ -0,0 +1,6 @@
+$("#sendMail").on("click", function () {
+    var name = $("#name").val();
+    var phone = $("#phone").val();
+    var message = $("#message").val();
+    var checkbox = $("#checkbox").val();
+});
